0d81861fa291eb9cd4566aa051aba771e1413151
ManOnTheMountainTech <mindinsky@gmail.com>
2020-10-29 15:12:15 -0700

    Now inserting two fields into dogpool

--- ironpaws/php/regADog.php
+++ ironpaws/php/regADog.php
@@ -5,26 +5,26 @@ $dogname = "";
 
 $numberError = FALSE;
 
-$servername = "supermooseapps.com";
+//$servername = "supermooseapps.com";
+$servername = "localhost";
 $user= "bryany_mushuser";
-$password = '!\\7JY3}K{dWn#M-+';
+//$password = '!\\7JY3}K{dWn#M-+';
+$password = 'BeebleBrox9!';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $bibnumber = test_number($_POST["bibNumber"]);
   $dogname = test_input($_POST["dogName"]);
-  $age = test_number($_POST["age"]);
-
-  echo $bibnumber . "<br>";
-  echo $dogname . "<br>";
-  echo $age . "<br>";
+  $dogage = test_number($_POST["dogAge"]);
+  $dogowner = test_input($_POST["dogOwner"]);
 
   try {
     echo "Connecting to musher database...please wait" . "<br>";
     $conn = new PDO("mysql:host=$servername;dbname=bryany_mush", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("INSERT INTO dogpool (name, age) VALUES ('$dogname', $dogage)");
   }
   catch(PDOException $e) {
-    echo "Connection to musher database failed: " . $e->getMessage();
+    die ( "Mush database error. The details are: " . $e->getMessage());
   }
 }
 
