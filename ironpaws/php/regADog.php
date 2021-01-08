319be04fbdd0711a1c4dd72f8189106005dfb0a6
ManOnTheMountainTech <mindinsky@gmail.com>
2021-01-08 14:43:49 -0800

    Got dogs to be registered.

--- ironpaws/php/regADog.php
+++ ironpaws/php/regADog.php
@@ -2,31 +2,44 @@
 // define variables and set to empty values
 $bibnumber = $age = 0;
 $dogname = "";
+$dogbirthday = "";
 
 $numberError = FALSE;
 
-//$servername = "supermooseapps.com";
-$servername = "localhost";
+$servername = "supermooseapps.com";
+//$servername = "localhost";
 $user= "bryany_mushuser";
-//$password = '!\\7JY3}K{dWn#M-+';
-$password = 'BeebleBrox9!';
+$password = '9E{y)E;32.Qep7%m';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $bibnumber = test_number($_POST["bibNumber"]);
+  //$bibnumber = test_number($_POST["bibNumber"]);
   $dogname = test_input($_POST["dogName"]);
-  $dogage = test_number($_POST["dogAge"]);
-  $dogowner = test_input($_POST["dogOwner"]);
+  if (empty($dogname)) {
+    die ($dogname . "is not a valid name.");
+  }
+
+  $dogbirthday = test_input($_POST["dogDOB"]);
+  if (empty($dogbirthday)) {
+    die ($dogbirthday . " is not a valid birth date.");
+  }
+
+  //$dogowner = test_input($_POST["dogOwner"]);
+
+  $execSql = "CALL NewDog ('{$dogname}', '{$dogbirthday}')";
+  echo "sql to execute: " . $execSql . "<BR>";
 
   try {
-    echo "Connecting to musher database...please wait" . "<br>";
+    echo "Connecting to musher database...please wait<br>";
     $conn = new PDO("mysql:host=$servername;dbname=bryany_mush", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("INSERT INTO dogpool (name, age) VALUES ('$dogname', $dogage)");
+    $conn->exec($execSql);
   }
   catch(PDOException $e) {
-    die ( "Mush database error. The details are: " . $e->getMessage());
+    die ( "The database returned an error while registering a dog. The details are: " . $e->getMessage());
   }
 }
+
+echo $dogname . " is sucessfully registered.<br>";
 
 // Returns: 0 if the number is invalid
 function test_number($number) {
