b1ae078d93b9fbd48a352b2a9b99e051911b6361
Bryan Young <bryany@supermooseapps.com>
2021-08-24 15:46:30 -0700

    Addition of Algorithms namespace

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -2,13 +2,17 @@
     defined( 'ABSPATH' ) || exit;
 
     spl_autoload_register(function ($class_name) {
-        // Generated from regex101.com
-        $split_class_name = explode('IronPaws\\', $class_name);
+        ifMatchingNamespaceLoad($class_name, 'IronPaws', '');
+        ifMatchingNamespaceLoad($class_name, 'Algorithms', 'algorithms/');
+    });
+
+    function ifMatchingNamespaceLoad(string $class_name, $namespace, string $pathFromInclude) {
+        $split_class_name = explode("{$namespace}\\", $class_name);
 
         if (isset($split_class_name[1])) {
             $pieces = explode('_', $split_class_name[1]);
             $proto_filename = implode('-', $pieces);
-            require_once strtolower($proto_filename) . '.php';
+            require_once $pathFromInclude . strtolower($proto_filename) . '.php';
         }
-    });
+    }
 ?>
