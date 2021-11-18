7f28b66032296f2e5ad777363f308efa4a408af2
Bryan Young <bryany@supermooseapps.com>
2021-11-18 14:40:19 -0800

    Updated install tranferer to be more consistent.

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 liveserver='/home/bryan/public_html/wp-content/plugins/ironpaws'
 read -p "Press any key to proceed with deleting live folders"
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/css
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/includes
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/vendor
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/tests
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/img
-rm -R /home/bryan/public_html/wp-content/plugins/ironpaws/tests
-rm /home/bryan/public_html/wp-content/plugins/ironpaws/*
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/css
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/includes
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/vendor
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/tests
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/img
+rm -R -f /home/bryan/public_html/wp-content/plugins/ironpaws/tests
+rm -f /home/bryan/public_html/wp-content/plugins/ironpaws/*
 
 mkdir /home/bryan/public_html/wp-content/plugins/ironpaws/css
 mkdir /home/bryan/public_html/wp-content/plugins/ironpaws/includes
@@ -18,8 +18,8 @@ mkdir /home/bryan/public_html/wp-content/plugins/ironpaws/tests
 
 cp *.php /home/bryan/public_html/wp-content/plugins/ironpaws/
 cp *.html /home/bryan/public_html/wp-content/plugins/ironpaws/
-cp -R css/* /home/bryan/public_html/wp-content/plugins/ironpaws/css/
-cp -R -v includes/ /home/bryan/public_html/wp-content/plugins/ironpaws/
-cp -R vendor/* /home/bryan/public_html/wp-content/plugins/ironpaws/vendor/
-cp -R img/* /home/bryan/public_html/wp-content/plugins/ironpaws/img/
-cp -R tests/* /home/bryan/public_html/wp-content/plugins/ironpaws/tests/
+cp -R css/ /home/bryan/public_html/wp-content/plugins/ironpaws/
+cp -R includes/ /home/bryan/public_html/wp-content/plugins/ironpaws/
+cp -R vendor/ /home/bryan/public_html/wp-content/plugins/ironpaws/
+cp -R img/ /home/bryan/public_html/wp-content/plugins/ironpaws/
+cp -R tests/ /home/bryan/public_html/wp-content/plugins/ironpaws/
